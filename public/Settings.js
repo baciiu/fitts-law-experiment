19c669bf5fee559c600d2425995e085bf534982f
Baciu Alexandra <60070747+baciiu@users.noreply.github.com>
2024-06-09 23:18:40 +0200

    [ISSUE #15] input parameter for center of the trial

--- public/Settings.js
+++ public/Settings.js
@@ -1,33 +1,33 @@
 const INPUT = [
   /*{ width: 6, height: 6 },
-        { width: 6, height: 10 },
-        { width: 6, height: 12 },
-        { width: 8, height: 8 },
-        { width: 8, height: 12 },*/
+          { width: 6, height: 10 },
+          { width: 6, height: 12 },
+          { width: 8, height: 8 },
+          { width: 8, height: 12 },*/
   { width: 10, height: 10 },
   { width: 10, height: 12 },
   //  { width: 12, height: 12 },
 
   /*{ width: 10, height: 10, angle: 0, amplitude: 100 },
-                            { width: 15, height: 15, angle: 180, amplitude: 100 },
-                            { width: 20, height: 20, angle: 0, amplitude: 100 },
-                            { width: 8, height: 8, angle: 180, amplitude: 100 },
-                            { width: 20, height: 20, angle: 0, amplitude: 100 },
-                            { width: 20, height: 20, angle: 90, amplitude: 100 },
-                            { width: 10, height: 20, angle: 270, amplitude: 100 },
-                            { width: 8, height: 8, angle: 180, amplitude: 100 },
-                            { width: 20, height: 20, angle: 90, amplitude: 100 },
-                            { width: 15, height: 15, angle: 270, amplitude: 100 },
-                            { width: 20, height: 10 },
-                            { width: 15, height: 15 },
-                            { width: 10, height: 20 },
-                            { width: 15, height: 10 },*/
+                              { width: 15, height: 15, angle: 180, amplitude: 100 },
+                              { width: 20, height: 20, angle: 0, amplitude: 100 },
+                              { width: 8, height: 8, angle: 180, amplitude: 100 },
+                              { width: 20, height: 20, angle: 0, amplitude: 100 },
+                              { width: 20, height: 20, angle: 90, amplitude: 100 },
+                              { width: 10, height: 20, angle: 270, amplitude: 100 },
+                              { width: 8, height: 8, angle: 180, amplitude: 100 },
+                              { width: 20, height: 20, angle: 90, amplitude: 100 },
+                              { width: 15, height: 15, angle: 270, amplitude: 100 },
+                              { width: 20, height: 10 },
+                              { width: 15, height: 15 },
+                              { width: 10, height: 20 },
+                              { width: 15, height: 10 },*/
 ];
 const AMPLITUDE_LIST = [40, 30];
 
 const ENABLE_FULL_SCREEN = false;
 
-const MAX_SCREEN_DISTANCE = mmToPixels(600);
+const MAX_SCREEN_DISTANCE = 40;
 
 const START_SIZE = 10; // if input has 2 parameters and discrete exp
 const RADIAN_START = 0;
@@ -47,7 +47,7 @@ const REPEAT_RECIPROCAL_GROUP = false;
 
 const SCRAMBLE_BLOCKS = true;
 
-const TRIALS_PER_BREAK = 7;
+const TRIALS_PER_BREAK = 20;
 const BLOCKS_NUMBER = 3;
 const REPETITION_PER_TRIAL = 4; // starting from 1
 
@@ -60,8 +60,8 @@ const REP_PRESS_IN_REL_IN = false;
 
 const USER = 5;
 
-const TOP_MARGIN_PX = mmToPixels(5);
-const OTHER_MARGINS_PX = mmToPixels(3);
+const TOP_MARGIN_PX = mmToPixels(10);
+const OTHER_MARGINS_PX = mmToPixels(5);
 const FAILED_TRIAL_THRESHOLD = 4; // divide amplitude by threshold
 const AMBIGUITY_MARGIN_PX = mmToPixels(20);
 
