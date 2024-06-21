3782ec7ca2afdce9bb74b7907a381bac1c5de8fb
Baciu Alexandra <60070747+baciiu@users.noreply.github.com>
2024-06-21 11:57:02 +0200

    [prod discrete] update settings

--- public/Settings.js
+++ public/Settings.js
@@ -1,27 +1,27 @@
 const INPUT = [
   /*{ width: 6, height: 6 },
-              { width: 6, height: 10 },
-              { width: 6, height: 12 },
-              { width: 8, height: 8 },
-              { width: 8, height: 12 },*/
+                  { width: 6, height: 10 },
+                  { width: 6, height: 12 },
+                  { width: 8, height: 8 },
+                  { width: 8, height: 12 },*/
   { width: 10, height: 10 },
   // { width: 10, height: 12 },
   //  { width: 12, height: 12 },
 
   /*{ width: 10, height: 10, angle: 0, amplitude: 100 },
-                                  { width: 15, height: 15, angle: 180, amplitude: 100 },
-                                  { width: 20, height: 20, angle: 0, amplitude: 100 },
-                                  { width: 8, height: 8, angle: 180, amplitude: 100 },
-                                  { width: 20, height: 20, angle: 0, amplitude: 100 },
-                                  { width: 20, height: 20, angle: 90, amplitude: 100 },
-                                  { width: 10, height: 20, angle: 270, amplitude: 100 },
-                                  { width: 8, height: 8, angle: 180, amplitude: 100 },
-                                  { width: 20, height: 20, angle: 90, amplitude: 100 },
-                                  { width: 15, height: 15, angle: 270, amplitude: 100 },
-                                  { width: 20, height: 10 },
-                                  { width: 15, height: 15 },
-                                  { width: 10, height: 20 },
-                                  { width: 15, height: 10 },*/
+                                      { width: 15, height: 15, angle: 180, amplitude: 100 },
+                                      { width: 20, height: 20, angle: 0, amplitude: 100 },
+                                      { width: 8, height: 8, angle: 180, amplitude: 100 },
+                                      { width: 20, height: 20, angle: 0, amplitude: 100 },
+                                      { width: 20, height: 20, angle: 90, amplitude: 100 },
+                                      { width: 10, height: 20, angle: 270, amplitude: 100 },
+                                      { width: 8, height: 8, angle: 180, amplitude: 100 },
+                                      { width: 20, height: 20, angle: 90, amplitude: 100 },
+                                      { width: 15, height: 15, angle: 270, amplitude: 100 },
+                                      { width: 20, height: 10 },
+                                      { width: 15, height: 15 },
+                                      { width: 10, height: 20 },
+                                      { width: 15, height: 10 },*/
 ];
 const AMPLITUDE_LIST = [100, 50];
 
@@ -33,32 +33,32 @@ const DIRECTION_LIST = [0];
 // 180 => [0,180]  // 2 directions
 
 const DEVICE_TYPE = DEV_TYPE.MOUSE;
-const EXPERIMENT_TYPE = EX_TYPE.RECIPROCAL;
+const EXPERIMENT_TYPE = EX_TYPE.DISCRETE;
 
 const START_HIT_NOT_MANDATORY = false; // works only with reciprocal exp
 
 const INTERRUPT_RECIPROCAL_GROUP = true;
 
-const SCRAMBLE_BLOCKS = false;
+const SCRAMBLE_BLOCKS = true;
 
 const TRIALS_PER_BREAK = 20;
 const BLOCKS_NUMBER = 3;
-const REPETITION_PER_TRIAL = 2; // starting from 1
+const REPETITION_PER_TRIAL = 3; // starting from 1
 
 const HIT_ON_PRESS_AND_RELEASE = false;
 
-const REP_PRESS_OUT_REL_OUT = false;
+const REP_PRESS_OUT_REL_OUT = true;
 const REP_PRESS_OUT_REL_IN = false;
 const REP_PRESS_IN_REL_OUT = false;
 const REP_PRESS_IN_REL_IN = false;
 
 const FAILED_TRIAL_THRESHOLD = 4; // divide amplitude by threshold
 
-const USER = 8;
+const USER = 9;
 
 const USE_CENTER_OF_SCREEN = false;
 
-const MAX_SCREEN_DISTANCE = 60;
+const MAX_SCREEN_DISTANCE = 70;
 
 const ENABLE_FULL_SCREEN = false;
 
