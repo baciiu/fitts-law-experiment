8b6bc434123ddcf133d49045391a1de955eecef9
Baciu Alexandra <60070747+baciiu@users.noreply.github.com>
2024-06-21 09:58:06 +0200

    [ISSUE #68] add variable for handling start

--- public/Settings.js
+++ public/Settings.js
@@ -1,33 +1,29 @@
 const INPUT = [
   /*{ width: 6, height: 6 },
-          { width: 6, height: 10 },
-          { width: 6, height: 12 },
-          { width: 8, height: 8 },
-          { width: 8, height: 12 },*/
+            { width: 6, height: 10 },
+            { width: 6, height: 12 },
+            { width: 8, height: 8 },
+            { width: 8, height: 12 },*/
   { width: 10, height: 10 },
   { width: 10, height: 12 },
   //  { width: 12, height: 12 },
 
   /*{ width: 10, height: 10, angle: 0, amplitude: 100 },
-                              { width: 15, height: 15, angle: 180, amplitude: 100 },
-                              { width: 20, height: 20, angle: 0, amplitude: 100 },
-                              { width: 8, height: 8, angle: 180, amplitude: 100 },
-                              { width: 20, height: 20, angle: 0, amplitude: 100 },
-                              { width: 20, height: 20, angle: 90, amplitude: 100 },
-                              { width: 10, height: 20, angle: 270, amplitude: 100 },
-                              { width: 8, height: 8, angle: 180, amplitude: 100 },
-                              { width: 20, height: 20, angle: 90, amplitude: 100 },
-                              { width: 15, height: 15, angle: 270, amplitude: 100 },
-                              { width: 20, height: 10 },
-                              { width: 15, height: 15 },
-                              { width: 10, height: 20 },
-                              { width: 15, height: 10 },*/
+                                { width: 15, height: 15, angle: 180, amplitude: 100 },
+                                { width: 20, height: 20, angle: 0, amplitude: 100 },
+                                { width: 8, height: 8, angle: 180, amplitude: 100 },
+                                { width: 20, height: 20, angle: 0, amplitude: 100 },
+                                { width: 20, height: 20, angle: 90, amplitude: 100 },
+                                { width: 10, height: 20, angle: 270, amplitude: 100 },
+                                { width: 8, height: 8, angle: 180, amplitude: 100 },
+                                { width: 20, height: 20, angle: 90, amplitude: 100 },
+                                { width: 15, height: 15, angle: 270, amplitude: 100 },
+                                { width: 20, height: 10 },
+                                { width: 15, height: 15 },
+                                { width: 10, height: 20 },
+                                { width: 15, height: 10 },*/
 ];
-const AMPLITUDE_LIST = [40, 30];
-
-const ENABLE_FULL_SCREEN = false;
-
-const MAX_SCREEN_DISTANCE = 40;
+const AMPLITUDE_LIST = [40, 100, 50];
 
 const START_SIZE = 10; // if input has 2 parameters and discrete exp
 const RADIAN_START = 0;
@@ -39,26 +35,30 @@ const DIRECTION_LIST = [0];
 const DEVICE_TYPE = DEV_TYPE.MOUSE;
 const EXPERIMENT_TYPE = EX_TYPE.RECIPROCAL;
 
-const USE_CENTER_OF_SCREEN = false;
+const START_HIT_NOT_MANDATORY = true; // for reciprocal
 
-const INTERRUPT_RECIPROCAL_GROUP = false;
-
-const REPEAT_RECIPROCAL_GROUP = false;
+const INTERRUPT_RECIPROCAL_GROUP = true;
 
 const SCRAMBLE_BLOCKS = true;
 
 const TRIALS_PER_BREAK = 20;
 const BLOCKS_NUMBER = 3;
-const REPETITION_PER_TRIAL = 4; // starting from 1
+const REPETITION_PER_TRIAL = 2; // starting from 1
 
 const HIT_ON_PRESS_AND_RELEASE = false;
 
-const REP_PRESS_OUT_REL_OUT = false;
-const REP_PRESS_OUT_REL_IN = true;
-const REP_PRESS_IN_REL_OUT = false;
+const REP_PRESS_OUT_REL_OUT = true;
+const REP_PRESS_OUT_REL_IN = false;
+const REP_PRESS_IN_REL_OUT = true;
 const REP_PRESS_IN_REL_IN = false;
 
-const USER = 5;
+const USER = 8;
+
+const USE_CENTER_OF_SCREEN = false;
+
+const MAX_SCREEN_DISTANCE = 60;
+
+const ENABLE_FULL_SCREEN = false;
 
 const TOP_MARGIN_PX = mmToPixels(10);
 const OTHER_MARGINS_PX = mmToPixels(5);
