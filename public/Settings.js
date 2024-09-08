218d7bbe6ba3446d1066d6b82ddddb1b5e03f1a7
Baciu Alexandra <60070747+baciiu@users.noreply.github.com>
2024-09-08 08:57:11 +0200

    [ISSUE #71] add variables for different input types

--- public/Settings.js
+++ public/Settings.js
@@ -1,35 +1,36 @@
-const INPUT = [{ width: 10, height: 10 }];
-const AMPLITUDE_LIST = [36, 20];
+const INPUT = test.INPUT_2;
+const AMPLITUDE_LIST = test.AMPL;
 
-const START_SIZE = 10; // if input has 2 parameters and discrete exp
+const START_SIZE = 10;
 const RADIAN_START = 0;
 const RADIAN_STEP = 180;
-const DIRECTION_LIST = [0, 22.5];
+const DIRECTION_LIST = test.ANGLES;
 // 90 => [0,90,180,270] // 4 directions
 
 const EXPERIMENT_TYPE = EX_TYPE.RECIPROCAL;
 
-const TRIALS_PER_BREAK = 20;
+const TRIALS_PER_BREAK = 100;
 
 /** SETTINGS FOR RECIPROCAL EXPERIMENT ONLY START */
 const START_HIT_NOT_MANDATORY = true;
 
 const INTERRUPT_RECIPROCAL_GROUP = true;
 
-const TRAVELS_NUMBER = 2;
+const TRAVELS_NUMBER = test.TRAVELS; // first one is the 0 travel
 
 /** SETTINGS FOR RECIPROCAL EXPERIMENT ONLY END */
 
 const SCRAMBLE_BLOCKS = false;
 
-const BLOCKS_NUMBER = 1;
+const BLOCKS_NUMBER = test.BLOCKS;
 
-const REPETITION_PER_TRIAL = 2; // starting from 1
-const FAILED_TRIAL_THRESHOLD = 4; // divide amplitude by threshold
+const REPETITION_PER_TRIAL = test.REP;
+
+const FAILED_TRIAL_THRESHOLD = 4;
 
 const HIT_ON_PRESS_AND_RELEASE = true;
 
-const REP_PRESS_OUT_REL_OUT = true;
+const REP_PRESS_OUT_REL_OUT = false;
 const REP_PRESS_OUT_REL_IN = false;
 const REP_PRESS_IN_REL_OUT = false;
 const REP_PRESS_IN_REL_IN = false;
@@ -46,4 +47,3 @@ const ENABLE_FULL_SCREEN = false;
 
 const TOP_MARGIN_PX = mmToPixels(10);
 const OTHER_MARGINS_PX = mmToPixels(5);
-const AMBIGUITY_MARGIN_PX = mmToPixels(20);
