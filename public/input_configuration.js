eebd2efea72b2b6ab051312d7f4ab468aee1df03
Baciu Alexandra <60070747+baciiu@users.noreply.github.com>
2024-12-13 17:43:58 +0100

    [ISSUE #50] refactorings

--- public/input_configuration.js
+++ public/input_configuration.js
@@ -1,3 +1,29 @@
+const test = Object.freeze({
+  TRIALS_PER_BREAK: 100,
+
+  BLOCKS: 2,
+
+  REP: 3, // +1 when counting
+
+  TRAVELS: 2, // +1 when counting
+
+  AMPL: [36],
+
+  ANGLES: [0, 12],
+
+  INPUT_2_PARAMS: [
+    { width: 10, height: 4 },
+    { width: 10, height: 4 },
+    // { width: 10, height: 4 },
+  ],
+
+  INPUT_4_PARAMS: [
+    { width: 10, height: 10, angle: 90, amplitude: 36 },
+    { width: 20, height: 20, angle: 180, amplitude: 54 },
+    //{ width: 12, height: 8, angle: 270, amplitude: 80 },
+  ],
+});
+
 const UIST20 = Object.freeze({
   // page 862 from " Modeling 2 Dimensional Touch Pointing "
 
